ba5e4699c835ccfd8bd352ac3829a09dce2ec731
Fabrício Godoy <skarllot@gmail.com>
2012-11-07 18:02:52 -0200

    Script code cleanup
    Created functions to split code in more meaningful way.
    Added comments to code readability.
    Modifications to native parameters parsing.

--- share/snmppass/snmpget.sh
+++ share/snmppass/snmpget.sh
@@ -1,110 +1,258 @@
 #!/bin/bash
+#
+# Script used as bridge between Net-SNMP and any structured (memory) filesystem.
+#
+# Copyright (C) 2012 Fabrício Godoy <skarllot@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+# Authors: Fabrício Godoy <skarllot@gmail.com>
+#
 
-[ ! `echo "$2" | grep "^$ROOT" | wc -l` -eq 1 ] && exit 1
-[ "$1" != "-g" ] && [ "$1" != "-n" ] && exit 1
+readonly PROG=snmpget.sh
+RETVAL=0
+VERBOSE=0
 
-TREE=`echo "$2" | sed -e "s/$ROOT//"`
+# Mandatory variables
+validate_vars() {
+    [ $VERBOSE -eq 1 ] && echo "Validating variables..."
 
-if [ "${TREE:0:1}" == "." ]; then
-   TREE=${TREE:1}
-fi
+    if [ -z $ROOT ]; then
+        [ $VERBOSE -eq 1 ] && echo "ROOT variable is not defined"
+        exit 1
+    fi
+    if [ -z $KEY_FILE ]; then
+        [ $VERBOSE -eq 1 ] && echo "KEY_FILE variable is not defined"
+        exit 1
+    fi
+    if [ ! -r $KEY_FILE ]; then
+        [ $VERBOSE -eq 1 ] && echo "$KEY_FILE file is not found"
+        exit 1
+    fi
 
-# Last node and Key node
-LNODE=`echo $TREE | awk -F. "{print \\$NF}"`
-KNODE=`echo $TREE | awk -F. "{out=\\$1; for(i=2;i<NF;i++){out=out\".\"\\$i}; print out}"`
+    # Exit if requested OID don't belong to this script
+    if [ ! $(echo "$OID" | grep "^$ROOT" | wc -l) -eq 1 ]; then
+        [ $VERBOSE -eq 1 ] && echo "The requested OID \"$OID\" is invalid"
+        exit 1
+    fi
+}
 
-KEYS_COUNT=`cat $KEY_FILE | wc -l`
-COUNT=`cat $KEY_FILE | grep "^$TREE\W" | grep -v "\\$LNODE"| wc -l`
-KEY=""
+key_load() {
+    [ $VERBOSE -eq 1 ] && echo "Loading KEY..."
 
-if [ $COUNT -eq 1 ]; then
-   KEY=`cat $KEY_FILE | grep "^$TREE\W" | grep -v "\\$LNODE"`
-   LNODE=""
-else
-   COUNT=`cat $KEY_FILE | grep "^$KNODE\W" | grep "\\$LNODE" | wc -l`
+    KEY_CONTENT=$(cat $KEY_FILE)
+    KEYS_COUNT=$(echo "$KEY_CONTENT" | wc -l)
+    # All single instance keys
+    KEY_CONTENT_SI=$(echo "$KEY_CONTENT" | grep -v "\$LNODE")
+    # All multiple instance keys
+    KEY_CONTENT_MI=$(echo "$KEY_CONTENT" | grep "\$LNODE")
 
-   if [ $COUNT -eq 1 ]; then
-      KEY=`cat $KEY_FILE | grep "^$KNODE\W" | grep "\\$LNODE"`
-   else  # LNODE is missing!
-      COUNT=`cat $KEY_FILE | grep "^$TREE\W" | grep "\\$LNODE" | wc -l`
+    # TREE var building
+    TREE=${OID//${ROOT}/}
+    [ "${TREE:0:1}" == "." ] && TREE=${TREE:1}
 
-      if [ $COUNT -eq 1 ]; then
-         KEY=`cat $KEY_FILE | grep "^$TREE\W" | grep "\\$LNODE"`
-         KNODE=$TREE
-         LNODE=""
-      fi
-   fi
-fi
+    # Last node and Key node
+    # Eg: TREE=1.2.3.4.5 -> KNODE=1.2.3.4 and LNODE=5
+    # LNODE var is reserved to multiple instances key, it indicates intance number.
+    LNODE=$(echo $TREE | awk -F. '{print $NF}')
+    KNODE=$(echo $TREE | awk -F. '{out=$1; for(i=2;i<NF;i++) \
+    {out=out"."$i}; print out}')
 
-LINENUM=`cat $KEY_FILE | grep -n "$KEY" | awk -F: "{print \\$1}"`
+    # Is a single instance?
+    COUNT=$(echo "$KEY_CONTENT_SI" | grep "^$TREE\W" | wc -l)
+    KEY=""
 
-# === SNMP NEXT ===
+    if [ $COUNT -eq 1 ]; then   # Single instance
+        KEY=$(echo "$KEY_CONTENT_SI" | grep "^$TREE\W")
+        LNODE=""
+    else    # Multiple instance
+        COUNT=$(echo "$KEY_CONTENT_MI" | grep "^$KNODE\W" | wc -l)
 
-if [ "$1" == "-n" ]; then
-   if [ -z "$KEY" ]; then
-      KEY=`cat $KEY_FILE | head -n 1`
-   else
-      CHKNEXT=0
-      COUNT=`echo "$KEY" | grep "\\$LNODE" | wc -l`
+        if [ $COUNT -eq 1 ]; then   # $TREE has $LNODE part
+            KEY=$(echo "$KEY_CONTENT_MI" | grep "^$KNODE\W")
+        else    # $LNODE part of $TREE is missing
+            COUNT=$(echo "$KEY_CONTENT_MI" | grep "^$TREE\W" | wc -l)
 
-      if [ $COUNT -eq 0 ]; then
-         LINENUM=$(( $LINENUM + 1 ))
-         LNODE=0
-         CHKNEXT=1
-      else
-         [ -z "$LNODE" ] && LNODE=0
+            if [ $COUNT -eq 1 ]; then
+                KEY=$(echo "$KEY_CONTENT_MI" | grep "^$TREE\W")
+                KNODE=$TREE
+                LNODE=""
+            else    # Requested key is not found
+                [ $VERBOSE -eq 1 ] && echo "The key \"$TREE\" is invalid"
+            fi
+        fi
+    fi
 
-         LNODE=$(( $LNODE + 1 ))
-         FILENAME=$(eval echo `echo $KEY | awk "{print \\$3}"`)
+    # Get line number where $KEY is found
+    LINENUM=$(echo "$KEY_CONTENT" | grep -n "$KEY" | awk -F: '{print $1}')
+}
 
-         if [ ! -f $FILENAME ]; then
+
+# GET request
+get() {
+    [ $VERBOSE -eq 1 ] && echo "GET response:"
+    if [ -z "$KEY" ]; then
+        [ $VERBOSE -eq 1 ] && echo "No key defined"
+        exit 1
+    fi
+
+    FILENAME=$(eval echo $(echo $KEY | awk '{print $3}'))
+    if [ ! -f $FILENAME ]; then
+        [ $VERBOSE -eq 1 ] && echo "The file \"$FILENAME\" is not found"
+        exit 1
+    fi
+
+    echo "${ROOT}.${TREE}"
+    echo $KEY | awk '{print $2}'
+    cat "$FILENAME"
+
+    return 0
+}
+
+# GETNEXT request
+getnext() {
+    [ $VERBOSE -eq 1 ] && echo "GETNEXT response:"
+
+    if [ -z "$KEY" ]; then
+        # No key beside ROOT, then pick the first one
+        KEY=$(echo "$KEY_CONTENT" | head -n 1)
+    else
+        # $LNODE keyword indicates that a property
+        # has multiple instances
+
+        CHKNEXT=0
+        # Has "$LNODE" keyword?
+        COUNT=$(echo "$KEY" | grep "\$LNODE" | wc -l)
+
+        if [ $COUNT -eq 0 ]; then
+            # No "$LNODE" keyword, then get next line
             LINENUM=$(( $LINENUM + 1 ))
             LNODE=0
             CHKNEXT=1
-         fi
-      fi
-
-      [ $LINENUM -gt $KEYS_COUNT ] && exit 1
-
-      if [ $CHKNEXT -eq 1 ]; then
-         RPT=1
-         while [ $RPT -eq 1 ]; do
-            RPT=0
-
-            # Get line from number
-            KEY=`cat $KEY_FILE | head -n $LINENUM | tail -n 1`
-            COUNT=`echo "$KEY" | grep "\\$LNODE" | wc -l`
-
-            if [ ! $COUNT -eq 0 ]; then
-               LNODE=$(( $LNODE + 1 ))
-               FILENAME=$(eval echo `echo $KEY | awk "{print \\$3}"`)
-
-               if [ ! -f $FILENAME ]; then
-                  LINENUM=$(( $LINENUM + 1 ))
-                  LNODE=0
-                  RPT=1
-               fi
+        else
+            if [ -z "$LNODE" ]; then
+                # If no $LNODE defined, set to 1
+                LNODE=1
+            else
+                # Set to next instance
+                LNODE=$(( $LNODE + 1 ))
             fi
 
-            [ $LINENUM -gt $KEYS_COUNT ] && exit 1
-         done
-      fi
+            FILENAME=$(eval echo $(echo $KEY | awk '{print $3}'))
 
-   fi
+            if [ ! -f $FILENAME ]; then
+                # Instance do not exist, then get next key
+                LINENUM=$(( $LINENUM + 1 ))
+                LNODE=0
+                CHKNEXT=1
+            fi
+        fi
 
-   TREE=`echo $KEY | awk "{print \\$1}"`
-   COUNT=`echo "$KEY" | grep "\\$LNODE" | wc -l`
-   [ ! $COUNT -eq 0 ] && TREE="${TREE}.${LNODE}"
-fi
+        if [ $LINENUM -gt $KEYS_COUNT ]; then
+            [ $VERBOSE -eq 1 ] && echo "Last key reached, exit"
+            exit 1
+        fi
 
-# === SNMP GET ===
+        # Need to check next key?
+        if [ $CHKNEXT -eq 1 ]; then
+            RPT=1
+            while [ $RPT -eq 1 ]; do
+                RPT=0
 
-[ -z "$KEY" ] && exit 1
+                # Get line from number
+                KEY=$(echo "$KEY_CONTENT" | head -n $LINENUM | tail -n 1)
+                COUNT=$(echo "$KEY" | grep "\$LNODE" | wc -l)
 
-FILENAME=$(eval echo `echo $KEY | awk "{print \\$3}"`)
-[ ! -f $FILENAME ] && exit 1
+                # Next key is multiple instance?
+                if [ ! $COUNT -eq 0 ]; then
+                    LNODE=$(( $LNODE + 1 ))
+                    FILENAME=$(eval echo $(echo $KEY | awk '{print $3}'))
 
-echo "${ROOT}.${TREE}"
-echo "`echo $KEY | awk "{print \\$2}"`"
-cat "$FILENAME"
+                    if [ ! -f $FILENAME ]; then
+                        # Instance do not exist, then get next key
+                        LINENUM=$(( $LINENUM + 1 ))
+                        LNODE=0
+                        RPT=1
+                    fi
+                fi
 
+                if [ $LINENUM -gt $KEYS_COUNT ]; then
+                    [ $VERBOSE -eq 1 ] && echo "Last key reached, exit"
+                    exit 1
+                fi
+            done
+        fi
+
+    fi
+
+    TREE=$(echo $KEY | awk '{print $1}')
+    COUNT=$(echo "$KEY" | grep "\$LNODE" | wc -l)
+    [ ! $COUNT -eq 0 ] && TREE="${TREE}.${LNODE}"
+}
+
+# Parameters parsing
+ARGS=$(getopt -o "vg:n:" -l "verbose,debug" -n "$PROG" -- "$@")
+[ $? -ne 0 ] && exit 1
+eval set -- "$ARGS"
+
+OPT=
+OID=
+while true; do
+    case "$1" in
+        -v|--verbose)
+            VERBOSE=1
+            shift
+            ;;
+        -g|-n)
+            if [ ! -z $OPT ]; then
+                echo "The parameters \"-g\" and \"-n\" cannot be defined both"
+                OPT="ERROR"
+                break
+            fi
+            OPT=$1
+            OID=$2
+            shift 2
+            ;;
+        --debug)
+            set -x
+            ;;
+        --)
+            shift
+            break
+            ;;
+    esac
+done
+
+case "$OPT" in
+    -n)
+        validate_vars
+        key_load
+        getnext
+        get
+        RETVAL=$?
+        ;;
+    -g)
+        validate_vars
+        key_load
+        get
+        RETVAL=$?
+        ;;
+    *)
+        echo "Usage: $0 [ --verbose ] [ {-g|-n} <OID> ]"
+        RETVAL=1
+esac
+
+exit $RETVAL
+
+# vim: set ts=4 sw=4 et
